4a84ac239441e91cab7229287695ede2b519f63e
jsdrever <jsdrever09@gmail.com>
2023-03-09 13:10:22 -0600

    solid progress almost completely working need to figure out update for sql

--- db/server.js
+++ db/server.js
@@ -16,91 +16,180 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: '',
-      database: 'employee_tracker_db'
+        host: '127.0.0.1',
+        // MySQL username,
+        user: 'root',
+        port: 3306,
+        // MySQL password
+        password: '',
+        database: 'employee_tracker_db'
     },
     console.log(`Connected to the employee_tracker_db database.`)
-  );
+);
 
-  
-  
-  // Default response for any other request (Not Found)
-  app.use((req, res) => {
-      res.status(404).end();
-    });
-    
-    const questions = [
-        {
-            type: 'list',
-            message: 'What would you like to do? (use arrow keys)',
-            name: 'contact',
-            choices: [
-                'Update Employee Role', 
-                'View All Roles', 
-                'Add Role',
-                'View All Departments',
-                'Add Department',
-                'QUIT',
-                'View All Employees',
-            ],
-        }];
-        
-        
-        // This writes the file and puts the data on the file
-        function writeToFile(fileName, data) { 
-            fs.writeFileSync(fileName, data, (err) =>
-            err ? console.error(err) : console.log('Success!')) 
-        }
-        // this function calls the questions/answers from inquirer
-        function init() {
-            inquirer.prompt(questions)
-            .then((answers) => {
-                // needs to respond to specific answers and potentially call more prompts/inputs for the add role,dept, and employee options.
-                //! then it will call specific parts of schema or other sql file to update or show requested data
-                // todo should i make seperate files to store each option of show and one to update files? if so be sure to require and export module
+db.connect(err => {
+    if (err) throw err
+})
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do? (use arrow keys)',
+        name: 'contact',
+        choices: [
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'QUIT',
+            'View All Employees',
+        ],
+    }];
+
+
+// This writes the file and puts the data on the file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!'))
+}
+// this function calls the questions/answers from inquirer
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+
+            if (answers.contact === 'View All Departments') {
+                viewDept()
+            } else if (answers.contact === 'View All Roles') {
+                viewRoles()
                 
-                if (answers.choices === 'View All Departments') {
-                    viewDept()
-                }
-                function viewDept() {
-                    // SELECT * FROM department
-                }
-                
+            } else if (answers.contact === 'View All Employees') {
+                viewEmps()
+            } else if (answers.contact === 'Add Department') {
+                addDept()
+            } else if (answers.contact === 'Add Role') {
+                addRole()
+            } else if (answers.contact === 'Update Employee Role') {
+                updateRole()
+            }
+            function viewDept() {
+                // SELECT * FROM department
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.table(results);
+                    if (err) console.err(err) 
+                    
+                    init();
+                });
+            }
+            function viewRoles() {
+                db.query('SELECT * FROM roles', function (err, results) {
+                    console.table(results);
+                    if (err) console.err(err)
+
+                    init();
+                });
+
+            }
+            function viewEmps() {
+                db.query('SELECT * FROM employee', function (err, results) {
+                    console.table(results);
+                    if (err) console.err(err);
+                    init();
+                });
+            }
+
+        })
+    function addDept() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'what is the name of the department?'
+            }
+        ])
+            .then(answers => {
+                db.query('INSERT INTO department(dept_name) VALUES ("' + answers.name + '")', function (err, results) {
+                    console.table(results);
+                    if (err) console.err(err);
+                })
             })
-            .then(() => {
-                console.log('we done it yo!');
-            }) 
-            .catch((error) => {
-                console.log(error);
-                console.log("wrong dummy");
-            });
-        }
-        
-        
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-        
-        init();
-        
+    }
+    function addRole() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the role?'
+            },
+            {
+                type: 'input',
+                name: 'dept_name',
+                message: 'Which department are you putting this role into?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary for this role?'
+            }
+        ])
+        .then(answers => {
+            db.query(`INSERT INTO roles(job_title, role_dept, salary) VALUES ("${answers.name}", "${answers.dept_name}", "${answers.salary}" )`, function (err, results) {
+                console.table(results);
+                if(err) console.err(err);
+                init();
+            })
+        })
+    }
+    function updateRole() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Which employee are you updating',
+                choices: [
+                    'Captain Ahab',
+                    'Quee Queg',
+                    'Moby Dick',
+                    'Ish Mael',
+                    'R.J. MacReady',
+                    'Ben Chang',
+                    'Eren Yeager',
+                    'Annie Edison',
+                ]
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Which role do you want to assign the selected employee?',
+                choices: [
+                    'Engineering',
+                    'Finance',
+                    'Legal',
+                    'Sales',
+                    'Services',
+                ]
+            }
+        ])
+        .then(answers => {
+            db.query(`UPDATE employee`)
+            //! how to I write updates??
+        })
+    }
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+init();
+
         // Query database for the node server.js
         // todo Do i need to make one for each table/option: dept, role, employee?
         //! make them the return for switch case on the inquirer prompts 
-        db.query('SELECT * FROM department', function (err, results) {
-            if (err) console.err(err)
-            else {
-                if (results.length > 0) {
-                    console.log(results);
-                } else {
-                    res(200, 'no results you dumb bastard');
-                }
-            }
-          });
-        db.query('SELECT * FROM employee', function (err, results) {
-          });
-        db.query('SELECT * FROM roles', function (err, results) {
-          });
+
--- schema.sql
+++ schema.sql
@@ -1,15 +1,17 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
 CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    role_id INT NOT NULL,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     job_title VARCHAR(30) NOT NULL,
     role_dept VARCHAR(30) NOT NULL,
     salary VARCHAR(30) NOT NULL
@@ -17,7 +19,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-    emp_id INT NOT NULL,
+    emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     job_title VARCHAR (30) NOT NULL,
